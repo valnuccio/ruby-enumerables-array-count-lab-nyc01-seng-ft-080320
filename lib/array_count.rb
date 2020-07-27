4fa91db2f6481058ce2bbff0f747bbbc4aa61785
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-27 17:55:11 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,7 +4,6 @@ def count_strings(array)
   end
 end
   # Return the total number of strings in the provided array using the count enumerable
-end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
